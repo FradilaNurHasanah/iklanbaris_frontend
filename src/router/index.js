51e244ae321c03a2fbeda201644c07983739b6b4
Fradila Nur Hasanah <fradilanurhasanah@gmail.com>
2020-03-27 20:42:40 +0700

    editlagi

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,70 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Category from '../views/Category.vue'
+import Product from '../views/Product.vue'
+import Confirmation from '../views/Confirmation.vue'
+import Cart from '../views/Confirmation.vue'
+import Navbar from '../views/layouts/Navbar.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    components: {default: Login}
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    components: {default: Register, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/home',
+    name: 'home',
+    components: {default: Home, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/category',
+    name: 'category',
+    components: {default: Category, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/product',
+    name: 'product',
+    components: {default: Product, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/confirmation',
+    name: 'confirmation',
+    components: {default: Confirmation, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    components: {default: Cart, header: Navbar},
+    meta: { 
+      requiresAuth: true
+    }
   }
 ]
 
---
+++ src/views/layouts/Navbar.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-11">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">Home</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Shop
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                        <a class="dropdown-item" href="category.html"> shop category</a>
+                                        <a class="dropdown-item" href="single-product.html">product details</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        pages
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
+                                        <a class="dropdown-item" href="login.html"> 
+                                            Sign In
+                                        </a>
+                                        <a class="dropdown-item" href="register.html">register</a>
+                                        <a class="dropdown-item" href="cart.html">shopping cart</a>
+                                        <a class="dropdown-item" href="confirmation.html">confirmation</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="hearer_icon d-flex">
+                            <div class="dropdown cart">
+                                <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="ti-bag"></i>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <div class="single_product">
+    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    computed : {
+        isLoggedIn : function(){ return this.$store.getters.isLoggedIn}
+    },
+    methods:{
+      logout:function(){
+          let conf = { headers : {"Authorization" : "Bearer " + localStorage.getItem("Authorization")} };
+          let form = new FormData();
+          this.axios.post('/logout', form, conf).then(response => {
+            if (response.data.logged === false || response.data.status === 0) {
+                this.$store.commit('logout')
+                localStorage.removeItem("Authorization")
+                this.$router.push({name: 'login'})
+            }
+          }).catch(error => {
+
+        });
+      },
+  },
+}
+</script>
